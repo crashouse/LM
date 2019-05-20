955924102cf108e6717e3559d18c41e813d2de78
Julio <zero@Julios-MBP.home>
2019-05-20 10:32:20 +0200

    Ejercicio2

--- ejercicio2.js
+++ ejercicio2.js
@@ -5,3 +5,23 @@ while (confirmacion!=true) {
 numero=prompt('Selecciona el primer numero', '1');
 confirmacion=confirm(`¿es ${numero} su eleccion?`);
   }
+numero2=prompt('Selecciona el segundo numero','1');
+confirmacion2=confirm('el segundo numero ha sido introducido');
+while (numero!=numero2) {
+    if (numero2>numero)
+
+    {
+    alert ('el numero es mayor');
+    numero2=prompt('Selecciona el segundo numero','1');
+
+  }
+  else {
+if (numero2<numero)
+    alert ('el numero es menor');
+    numero2=prompt('Selecciona el segundo numero','1');
+  }
+
+
+}
+alert('has acertado');
+alert ('Enhorabuena');
