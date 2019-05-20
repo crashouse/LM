fe7dbe3998646bd8480ab55f8f1dcc1a6c167929
Julio <zero@Julios-MBP.home>
2019-05-20 10:26:21 +0200

    asir

---
+++ asir.js
@@ -0,0 +1,21 @@
+let alumnoAsir {
+
+notaPAR: PAR,
+notaFH : FH,
+notaLM : LM,
+notaISO : ISO,
+notaBD : BD,
+notaFOL : FOL,
+notaDescanso : Descanso
+
+};
+
+for (let nota in alumnoAsir) {
+  alumnoAsir[nota]=prompt ("Introduce la nota de  "+nota);
+
+};
+
+for (alumnoAsir) {
+alert (alumnoAsir.nota);
+
+}
