07d4a1f3cf0ba920752acb1497c3cf03b8822913
Julio <zero@Julios-MBP.home>
2019-05-29 09:56:08 +0200

    colores

---
+++ colores.js
@@ -0,0 +1,40 @@
+
+let ol=document.getElementsByTagName('ol');
+for (elemol of ol){
+//					let divs = document.getElementsByTagName('li');
+divs=elemol.children;
+let contador=0;
+for (elemdiv of divs) {
+if (!(contador%2)) {elemdiv.style.background='red';
+} else {
+elemdiv.style.background='blue';
+
+}
+contador++;
+}}
+
+let ul=document.getElementsByTagName('ul');
+for (elemul of ul){
+//					let divs = document.getElementsByTagName('li');
+divs=elemul.children;
+let contador=0;
+for (elemdiv of divs) {
+if (!(contador%2)) {
+  elemdiv.style.background='green';
+} else {
+elemdiv.style.background='blue';
+
+}
+contador++;
+}}
+
+
+
+
+conocimientos.style.background='orange';
+
+let tablas = document.getElementsByTagName('table');
+for (tablita in tablas) {
+tablita.row[0].cells[0].style.background='pink';
+
+};
